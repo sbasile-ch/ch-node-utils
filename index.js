4f85ac136dd5e8cf478fb555fc7e7ab1f4aaffbe
sbasile-ch <sbasile@companieshouse.gov.uk>
2023-08-30 07:49:41 +0100

    test

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-import { getSubDirs } from './utils/subDirs.js';
-import {
-     getNamesArray,
-     getNativeNamesArray,
-     getNamesObjectArray,
-     getNativeNamesObjectArray,
-     sourceLocales,
-     isSupportedLocale } from './utils/languageNames.js';
-import { addData }       from './utils/Locales.js';
+import { getSubDirs as getSubDirs} from './utils/subDirs.js';
+import { addData as addData}       from './utils/Locales.js';
+import { getNamesArray             as getNamesArray             } from './utils/languageNames.js';
+import { getNativeNamesArray       as getNativeNamesArray       } from './utils/languageNames.js';
+import { getNamesObjectArray       as getNamesObjectArray       } from './utils/languageNames.js';
+import { getNativeNamesObjectArray as getNativeNamesObjectArray } from './utils/languageNames.js';
+import { sourceLocales             as sourceLocales             } from './utils/languageNames.js';
+import { isSupportedLocale         as isSupportedLocale         } from './utils/languageNames.js';
 
 const chNodeUtils = {
     getSubDirs,
