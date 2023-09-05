20db1efcbee844351a85bb03fdf8393fda4266b7
sbasile-ch <sbasile@companieshouse.gov.uk>
2023-09-05 09:51:32 +0100

    singleton rework for i18nCh

--- src/utils/i18nCh.ts
+++ src/utils/i18nCh.ts
@@ -1,3 +1,4 @@
+// import i18next, { i18n } from 'i18next'
 import i18next from 'i18next'
 import Backend from 'i18next-fs-backend'
 import path    from 'path';
@@ -5,56 +6,130 @@ import fs, { readdirSync, lstatSync } from 'fs'
 
 export default class i18nCh {
 
-   // load all the file names (excluded .json) present in a certain dir
-   private static _loadAllNamespaces (localesFolder: string): string[] {
-      const jsonFiles: string[] = [];
+   private static instance: i18nCh
+   private i18nInst
+   private localesFolder
+   private nameSpaces: string[] = [];
 
-      fs.readdirSync(localesFolder).forEach((file) => {
-               if (path.extname(file) === '.json') {
-                  jsonFiles.push (path.basename(file, '.json'));
-               }
-         });
-   return jsonFiles;
+   //_______________________________________________________________________________________________
+   private constructor(localesFolder = "", nameSpaces: string[] = [], lang = "en") {
+      try {
+         if (!localesFolder) {
+            // no point to translate these errors as if they should ever happen they are input for devs, not customers
+            throw new Error("i18nCh initialization error: path to locales must be provided")
+         }
+         this.localesFolder = localesFolder
+         if ( nameSpaces.length === 0) {
+            nameSpaces =  this.loadAllNamespaces();
+         }
+         this.nameSpaces = nameSpaces
+
+         this.i18nInst = i18next
+         this.i18nInst
+            .use(Backend)
+            .init({
+            initImmediate: false, // false = will load the resources synchronously
+            ns: nameSpaces,
+            partialBundledLanguages: true,
+            lng: lang,
+            fallbackLng: 'en',
+            preload: readdirSync(localesFolder).filter((fileName) => {
+               const joinedPath = path.join(localesFolder, fileName)
+               return lstatSync(joinedPath).isDirectory()
+            }),
+            backend: {
+               loadPath: path.join(localesFolder, `{{lng}}/{{ns}}.json`)
+            }
+            })
+      }
+      catch (err) {
+         throw err; // propagate
+      }
+   }
+   //_______________________________________________________________________________________________
+   // Singleton retriever
+   public static getInstance(localesFolder?: string, nameSpaces?: string[], lang?: string): i18nCh {
+      if (!i18nCh.instance) {
+         i18nCh.instance = new i18nCh(localesFolder, nameSpaces, lang );
+      }
+      return i18nCh.instance;
    }
 
+   //_______________________________________________________________________________________________
+   // load all the file names (excluded .json) present in a certain dir
+   private loadAllNamespaces (): string[] {
+      const jsonFiles: string[] = [];
+
+      if (this.localesFolder) {
+         fs.readdirSync(path.join(this.localesFolder, 'en')).forEach((file) => {   // use 'en' as the only guaranteed to exist
+            if (path.extname(file) === '.json') {
+                     jsonFiles.push (path.basename(file, '.json'));
+                  }
+            });
+      }
+      return jsonFiles;
+   }
+   //_______________________________________________________________________________________________
+   // load all the file names (excluded .json) present in a certain dir
+   private changeLanguage (lang: string) {
+      this.i18nInst.changeLanguage(lang, (err) => {
+          if (err) throw new Error(`i18nCh changeLanguage error: ${err}`)
+      });
+   }
+
+   //_______________________________________________________________________________________________
    // load all the file names (excluded extension: .json) present in a certain dir
-   static addTranslations (localesFolder: string, lang: string, nameSpaces: string[], vars: any = {}) {
-      // let data: keyPairs = {};
+   public resolveNamespacesKeys (lang: string, vars: any = {}) {
       let data: any = {};
 
-      if (nameSpaces.length === 0) {
-         nameSpaces =  i18nCh._loadAllNamespaces(path.join(localesFolder, 'en'));
+      try {
+          if (this.i18nInst && this.nameSpaces ) {
+             this.changeLanguage (lang)
+             const keysValuesList = this.i18nInst.getDataByLanguage('en'); // use 'en' as the only guaranteed to exist
+             if ( keysValuesList !== undefined) {
+                for (const [ns, value] of Object.entries(keysValuesList)) {
+                   console.log(`${ns}: ${this.nameSpaces}`);
+                   if (this.nameSpaces.includes(ns)) {
+                      console.log(`${ns}`);
+                      for (const [key] of Object.entries(value)) {
+                         console.log(`${key}`);
+                         data[key] = this.i18nInst.t(key, {lng: lang, ns: ns, vars});
+                      }
+                   }
+                }
+             }
+          }
       }
-      i18next
-      .use(Backend)
-      .init({
-      initImmediate: false, // setting initImediate to false, will load the resources synchronously
-      ns: nameSpaces,
-      lng: lang,
-      fallbackLng: 'en',
-      preload: readdirSync(localesFolder).filter((fileName) => {
-         const joinedPath = path.join(localesFolder, fileName)
-         return lstatSync(joinedPath).isDirectory()
-      }),
-      backend: {
-         loadPath: path.join(localesFolder, `{{lng}}/{{ns}}.json`)
-      }
-      })
-
-      const keysValuesList = i18next.getDataByLanguage('en');
-      if ( keysValuesList !== undefined) {
-         for (const [ns, value] of Object.entries(keysValuesList)) {
-            console.log(`${ns}: ${nameSpaces}`);
-            if (nameSpaces.includes(ns)) {
-               console.log(`${ns}`);
-               for (const [key] of Object.entries(value)) {
-                  console.log(`${key}`);
-                  data[key] = i18next.t(key, {ns: ns, vars});
-               }
-            }
-         }
+      catch (err) {
+         throw err; // propagate
       }
 
       return data;
+   }
+   //_______________________________________________________________________________________________
+   // resolve 1 single key
+   public resolveSingleKey (key: string, lang: string, vars: any = {}): string {
+      let t = key
+          if (this.i18nInst) {
+              try {
+                      this.changeLanguage (lang)
+                      t = this.i18nInst.t(key, {lng: lang, ns: this.nameSpaces, vars})
+                      console.log(`searched for key:${key} lang:${lang} and got: ${t}`)
+              }
+              catch (err) {
+                 throw err; // propagate
+              }
+      }
+      return t
+   }
+   //_______________________________________________________________________________________________
+   // load further Namespaces
+   public loadNamespaces (nameSpaces: string[] = [])  {
+      if ( nameSpaces.length > 0 && this.i18nInst) {
+         this.i18nInst.loadNamespaces(nameSpaces, (err) => {
+            // no point to translate these errors as if they should ever happen they are input for devs, not customers
+            throw new Error(`i18nCh loadNamespaces error - unable to load namespaces: ${nameSpaces} - ${err}`)
+         })
+      }
    }
 }
